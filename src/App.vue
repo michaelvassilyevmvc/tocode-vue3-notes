46daba7ad3688e156c660451cb9cc96d077ec93d
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-15 13:36:10 +0500

    Init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <HeaderItem />
+    <div class="wrapper-content">
+      <div class="container">
+        <router-view />
+      </div>
+    </div>
+    <FooterItem />
+  </div>
+</template>
+
+<script>
+import HeaderItem from '@/components/HeaderItem.vue'
+import FooterItem from '@/components/FooterItem.vue'
+
+export default {
+  components: { HeaderItem, FooterItem }
+}
+</script>
+
+<style></style>
---
+++ src/components/FooterItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="container">
+      <ul class="navbar-list">
+        <li class="navbar-item" v-for="link in links" :key="link.alias">
+          <router-link class="navbar-link" :to="link.url">{{
+            link.title
+          }}</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { links } from '@/_config.js'
+
+export default {
+  data() {
+    return {
+      links: links
+    }
+  }
+}
+</script>
