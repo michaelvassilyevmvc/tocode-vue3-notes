8173f23f630f7fa4bceeae9bb8cf52d095c872d5
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-16 20:03:28 +0500

    Реализовано сохранение в localStorage и notes через watch

--- src/components/UI/TagsList.vue
+++ src/components/UI/TagsList.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="tags-list">
-    <div class="tag-item" v-for="(item) in items" :key="item"
-         @click="$emit('onItemClick',item)">
+    <div class="tag-item"
+         v-for="(item) in items"
+         :key="item"
+         @click="$emit('onItemClick',item)"
+         :class="{isPreview:isPreview}"
+    >
       <span>{{ item }}</span>
     </div>
   </div>
@@ -13,12 +17,17 @@ export default {
     items: {
       type: Array,
       required: true
+    },
+    isPreview: {
+      type: Boolean,
+      default: false
     }
   },
 }
 </script>
 
 <style lang="scss" scoped>
+
 .tags-list {
   padding: 10px 0;
   display: flex;
@@ -31,6 +40,16 @@ export default {
   background-color: #fff;
   border-radius: 22px;
   user-select: none;
+
+  &.isPreview {
+    padding: 0;
+    color: #444ce0;
+    cursor: default;
+
+    &:before {
+      content: '#'
+    }
+  }
 
   &:last-child {
     margin-right: 0;
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -12,15 +12,44 @@ export default {
   components: {Form, List},
   data() {
     return {
-      notes: ['task1', 'task2', 'task3',]
+      notes: [{
+        title: 'Learn Vue 3',
+        tags: ['work']
+      },
+        {
+          title: 'Finish course',
+          tags: ['work', 'home']
+        }
+      ]
+    }
+  },
+  mounted() {
+    this.getNotes();
+  },
+  watch: {
+    notes: {
+      handler(updatedList) {
+        localStorage.setItem('notes', JSON.stringify(updatedList))
+      },
+      deep: true,
     }
   },
   methods: {
-    handleSubmit(note) {
+    handleSubmit(title) {
+      const note = {
+        title: title,
+        tags: []
+      };
       this.notes.push(note);
     },
     handleRemove(index) {
       this.notes.splice(index, 1);
+    },
+    getNotes() {
+      const localNotes = localStorage.getItem('notes');
+      if (localNotes) {
+        this.notes = JSON.parse(localNotes);
+      }
     }
   }
 }
