6891a868e092e9808eb5f4412db22c02cbfcc12b
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-16 21:38:23 +0500

    Старт проетка для Vuex

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,56 +1,13 @@
 <template>
-  <div class="md-body">
-    <Form @onSubmit="handleSubmit"/>
-    <List @onRemove="handleRemove" :items="notes"/>
-  </div>
+  <p>vuex</p>
 </template>
-<script>
-import Form from '@/components/Notes/Form.vue'
-import List from '@/components/Notes/List.vue'
 
+<script>
 export default {
-  components: {Form, List},
-  data() {
-    return {
-      notes: [{
-        title: 'Learn Vue 3',
-        tags: ['work']
-      },
-        {
-          title: 'Finish course',
-          tags: ['work', 'home']
-        }
-      ]
-    }
-  },
-  mounted() {
-    this.getNotes();
-  },
-  watch: {
-    notes: {
-      handler(updatedList) {
-        localStorage.setItem('notes', JSON.stringify(updatedList))
-      },
-      deep: true,
-    }
-  },
-  methods: {
-    handleSubmit(title) {
-      const note = {
-        title: title,
-        tags: []
-      };
-      this.notes.push(note);
-    },
-    handleRemove(index) {
-      this.notes.splice(index, 1);
-    },
-    getNotes() {
-      const localNotes = localStorage.getItem('notes');
-      if (localNotes) {
-        this.notes = JSON.parse(localNotes);
-      }
-    }
-  }
+
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
